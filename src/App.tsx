a05e14769ae913fbc2731ffd495e8a52576222f6
Ayush Watnani <watnaniayush@gmail.com>
2023-06-22 23:20:03 +0530

    started simple price grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // import { useState } from 'react'
 import './App.css'
+import SimplePriceGrid from './components/simple_price_grid/SimplePriceGrid'
 // import ColumnPreview from './components/3_column_preview/ColumnPreview'
-import ProfileCard from './components/profile_card_component/ProfileCard'
+// import ProfileCard from './components/profile_card_component/ProfileCard'
 // import OrderSummary from './components/Order summary component/Order Summary/OrderSummary'
 // import Rating from './components/interactive_rating/Rating/Rating'
 // import StatsPreview from './components/stats_preview_card_component/StatsPreview'
@@ -14,7 +15,8 @@ function App() {
       {/* <OrderSummary /> */}
       {/* <StatsPreview /> */}
       {/* <ColumnPreview /> */}
-      <ProfileCard />
+      {/* <ProfileCard /> */}
+      <SimplePriceGrid />
     </>
   )
 }
---
+++ src/components/simple_price_grid/SimplePriceGrid.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './SimplePriceGrid.css';
+
+const SimplePriceGrid = () => {
+    return (
+        <div className='body'>
+            <div className="container">
+                <div className="wrapper">
+                    <div className="box join">
+                        <h1>Join our community</h1>
+                        <h3>30-day, hassle-free money back guarantee</h3>
+                        <p>Gain access to our full library of tutorials along with expert code reviews.</p>
+                        <p>Perfect for any developers who are serious about honing their skills.</p>
+                    </div>
+                    <div className="box sub">
+                        <h2>Monthly Subscription</h2>
+                        <h5><span className="price"> &dollar;29</span> per month</h5>
+                        <p>Full access for less than &dollar;1 a day</p>
+                        <a href="">Sign up</a>
+                    </div>
+                    <div className="box why">
+                        <h2>Why Us</h2>
+                        <ul>
+                            <li>Tutorials by industry experts</li>
+                            <li>Peer &amp; expert code review</li>
+                            <li>Coding exercises</li>
+                            <li>Access to our GitHub repos</li>
+                            <li>Community forum</li>
+                            <li>Flashcard decks</li>
+                            <li>New videos every week</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SimplePriceGrid
