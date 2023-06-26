2b79cd64a92095cd1d6d8535a4ff51fff59d25e2
Ayush Watnani <watnaniayush@gmail.com>
2023-06-26 23:24:33 +0530

    started advice generator app

---
+++ src/components/advice_generator_app/AdviceGenerator.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AdviceGenerator.css'
+
+const AdviceGenerator = () => {
+  return (
+    <div>AdviceGenerator</div>
+  )
+}
+
+export default AdviceGenerator
--- src/components/qr_code/QRCode.tsx
+++ src/components/qr_code/QRCode.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import './QRCode.css'
 
-const QRCode = () => {
-    console.log('sad')
+const QRCode = () => {    
     return (
         <div className='body'>
             <div className="container">
